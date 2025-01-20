772d6d711be6642333333df0bbc3041418d19c89
Jade Hamilton <jade@alchemy.com>
2025-01-20 16:02:42 -0700

    chore: make images fit

--- packages/nextjs/components/wizard/Wizard.tsx
+++ packages/nextjs/components/wizard/Wizard.tsx
@@ -9,16 +9,16 @@ export function Wizard(props: WizardInfo): any {
       <>
         <p>{props.name}</p>
 
-        <div key="harryPotter" style={{ height: "20rem", width: "15rem" }}>
+        <div key="harryPotter">
           <Image src="/frame1.jpg" alt="frame" width={240} height={320} style={{ maxWidth: "none" }} />
           <Image
             className="z-10 relative"
             src={props.imageUrl}
             key="harryPotter"
             alt="nft"
-            width={112}
-            height={149}
-            style={{ maxWidth: "none", left: "63px", top: "-170px" }}
+            width={109}
+            height={112}
+            style={{ maxWidth: "none", left: "66px", top: "-170px", height: "112px", width: "109px" }}
           />
         </div>
       </>
@@ -34,9 +34,9 @@ export function Wizard(props: WizardInfo): any {
           src={props.imageUrl}
           key="harryPotter"
           alt="nft"
-          width={106}
-          height={155}
-          style={{ maxWidth: "none", left: "80px", top: "-174px" }}
+          width={94}
+          height={116}
+          style={{ maxWidth: "none", left: "80px", top: "-174px", height: "116px", width: "94px" }}
         />
       </div>
     </>
