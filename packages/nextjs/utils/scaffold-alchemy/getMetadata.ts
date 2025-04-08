008a223a68d924e68801fada3764dab91d21be73
Dan Nolan <dan.nolan@alchemy.com>
2025-04-08 16:20:43 -0400

    fixes some potential build issues with vercel

--- packages/nextjs/utils/scaffold-alchemy/getMetadata.ts
+++ packages/nextjs/utils/scaffold-alchemy/getMetadata.ts
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 
 const baseUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : `http://localhost:${process.env.PORT}`;
+  : `http://localhost:${process.env.PORT || 56900}`;
 const titleTemplate = "%s | Scaffold-Alchemy";
 
 export const getMetadata = ({
