ae1c0a935826898532912abc9ef8c71c2b2fdecd
Dan Nolan <dan.nolan@alchemy.com>
2025-04-29 12:53:32 -0400

    refactor: moves chain utils to a shared workspace

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -11,7 +11,7 @@ import "hardhat-deploy-ethers";
 import { task } from "hardhat/config";
 import generateTsAbis from "./scripts/generateTsAbis";
 import cw3dConfig from "./cw3d.config";
-import { allChains } from "./utils/chainUtils";
+import { allChains } from "@scaffold-alchemy/shared";
 
 export const hardhatAccount0PrivateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
